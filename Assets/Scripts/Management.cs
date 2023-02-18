fdc9b73fbcfc7f0238b368be0affea1be02cb061
mishapushka <72805598+mishapushka@users.noreply.github.com>
2023-02-18 14:50:40 +0700

    Add barack. Create prefab

--- Assets/Scripts/Management.cs
+++ Assets/Scripts/Management.cs
@@ -2,95 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Management : MonoBehaviour
-{
+public class Management : MonoBehaviour {
 
     public Camera Camera;
     public SelectableObject Hovered;
     public List<SelectableObject> ListOfSelected = new List<SelectableObject>();
 
-    void Update()
-    {
+    void Update() {
 
         Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
         Debug.DrawRay(ray.origin, ray.direction * 10f, Color.red);
 
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {
-            if (hit.collider.GetComponent<SelectableColider>())
-            {
+        if (Physics.Raycast(ray, out hit)) {
+            if (hit.collider.GetComponent<SelectableColider>()) {
                 SelectableObject hitSelectable = hit.collider.GetComponent<SelectableColider>().SelectableObject;
-                if (Hovered)
-                {
-                    if (Hovered != hitSelectable)
-                    {
+                if (Hovered) {
+                    if (Hovered != hitSelectable) {
                         Hovered.OnUnHover();
                         Hovered = hitSelectable;
                         Hovered.OnHover();
                     }
-                }
-                else
-                {
+                } else {
                     Hovered = hitSelectable;
                     Hovered.OnHover();
                 }
-            }
-            else
-            {
+            } else {
                 UnHoverCurrent();
             }
-        }
-        else
-        {
+        } else {
             UnHoverCurrent();
         }
 
-        if (Input.GetMouseButtonUp(0))
-        {
-            if (Hovered)
-            {
-                UnSelectAll();
+        if (Input.GetMouseButtonUp(0)) {
+            if (Hovered) {
+                if (Input.GetKey(KeyCode.LeftControl) == false) {
+                    UnSelectAll();
+                }
+ 
                 Select(Hovered);
             }
 
-            if (hit.collider.tag == "Ground")
-            {
-                for (int i = 0; i < ListOfSelected.Count; i++)
-                {
+            if (hit.collider.tag == "Ground") {
+                for (int i = 0; i < ListOfSelected.Count; i++) {
                     ListOfSelected[i].WhenClickOnGround(hit.point);
                 }
             }
         }
 
-        if (Input.GetMouseButtonDown(1))
-        {
+        if (Input.GetMouseButtonDown(1)) {
             UnSelectAll();
         }
     }
 
-    void Select(SelectableObject selectableObject)
-    {
-        if (ListOfSelected.Contains(selectableObject) == false)
-        {
+    void Select(SelectableObject selectableObject) {
+        if (ListOfSelected.Contains(selectableObject) == false) {
             ListOfSelected.Add(selectableObject);
             selectableObject.Select();
         }
     }
 
-    void UnSelectAll()
-    {
-        for (int i = 0; i < ListOfSelected.Count; i++)
-        {
+    void UnSelectAll() {
+        for (int i = 0; i < ListOfSelected.Count; i++) {
             ListOfSelected[i].UnSelect();
         }
         ListOfSelected.Clear();
     }
 
-    void UnHoverCurrent()
-    {
-        if (Hovered)
-        {
+    void UnHoverCurrent() {
+        if (Hovered) {
             Hovered.OnUnHover();
             Hovered = null;
         }
--- Assets/Scripts/SelectableObject.cs
+++ Assets/Scripts/SelectableObject.cs
@@ -2,37 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SelectableObject : MonoBehaviour
-{
+public class SelectableObject : MonoBehaviour {
     public GameObject SelectionIndicator;
 
-    private void Start()
-    {
+    private void Start() {
         SelectionIndicator.SetActive(false);
     }
 
-    public virtual void OnHover()
-    {
+    public virtual void OnHover() {
         transform.localScale = Vector3.one * 1.1f;
     }
 
-    public virtual void OnUnHover()
-    {
+    public virtual void OnUnHover() {
         transform.localScale = Vector3.one;
     }
 
-    public virtual void Select()
-    {
+    public virtual void Select() {
         SelectionIndicator.SetActive(true);
     }
 
-    public virtual void UnSelect()
-    {
+    public virtual void UnSelect() {
         SelectionIndicator.SetActive(false);
     }
 
-    public virtual void WhenClickOnGround(Vector3 point)
-    {
+    public virtual void WhenClickOnGround(Vector3 point) {
 
     }
 }
