ce62e381352b4d36559ac665a30e83fa00d79b75
mishapushka <72805598+mishapushka@users.noreply.github.com>
2023-02-18 19:56:35 +0700

    Add tag

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Unit : SelectableObject
-{
-    //public NavMeshAgent NavMeshAgent;
-    //public override void WhenClickOnGround(Vector3 point)
-    //{
-    //    base.WhenClickOnGround(point);
+public class Unit : SelectableObject {
+    public NavMeshAgent NavMeshAgent;
+    public override void WhenClickOnGround(Vector3 point) {
+        base.WhenClickOnGround(point);
 
-    //    NavMeshAgent.SetDestination(point);
+        NavMeshAgent.SetDestination(point);
+    }
 }
