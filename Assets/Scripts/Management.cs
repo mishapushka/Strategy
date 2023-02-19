37eb0cb4d984d5143d9cdb6a1d530e6edecf2304
mishapushka <72805598+mishapushka@users.noreply.github.com>
2023-02-19 20:43:19 +0700

    Added border selection

--- Assets/Scripts/Management.cs
+++ Assets/Scripts/Management.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum SelectionState {
+    UnitsSelected,
+    Frame,
+    Other
+}
 
 public class Management : MonoBehaviour {
 
     public Camera Camera;
     public SelectableObject Hovered;
     public List<SelectableObject> ListOfSelected = new List<SelectableObject>();
+
+    public Image FrameImage;
+    private Vector2 _frameStart;
+    private Vector2 _frameEnd;
+
+    public SelectionState CurrentSelectionState;
 
     void Update() {
 
@@ -39,19 +52,70 @@ public class Management : MonoBehaviour {
                 if (Input.GetKey(KeyCode.LeftControl) == false) {
                     UnSelectAll();
                 }
- 
+                CurrentSelectionState = SelectionState.UnitsSelected;
                 Select(Hovered);
             }
+        }
 
-            if (hit.collider.tag == "Ground") {
-                for (int i = 0; i < ListOfSelected.Count; i++) {
-                    ListOfSelected[i].WhenClickOnGround(hit.point);
+        // здесь можно делать хоть что, когда юниты выделены
+        if (CurrentSelectionState == SelectionState.UnitsSelected) {
+
+            if (Input.GetMouseButtonUp(0)) {
+                if (hit.collider.tag == "Ground") {
+                    for (int i = 0; i < ListOfSelected.Count; i++) {
+                        ListOfSelected[i].WhenClickOnGround(hit.point);
+                    }
                 }
             }
         }
 
+
         if (Input.GetMouseButtonDown(1)) {
             UnSelectAll();
+        }
+
+        // Выделение рамкой
+        if (Input.GetMouseButtonDown(0)) {
+            _frameStart = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0)) {
+            _frameEnd = Input.mousePosition;
+
+            Vector2 min = Vector2.Min(_frameStart, _frameEnd);
+            Vector2 max = Vector2.Max(_frameStart, _frameEnd);
+            Vector2 size = max - min;
+
+            if (size.magnitude > 10) {
+
+                FrameImage.enabled = true;
+
+                FrameImage.rectTransform.anchoredPosition = min;
+
+                FrameImage.rectTransform.sizeDelta = size;
+
+                Rect rect = new Rect(min, size);
+
+                UnSelectAll();
+                Unit[] allUnits = FindObjectsOfType<Unit>();
+                for (int i = 0; i < allUnits.Length; i++) {
+                    Vector2 screenPosition = Camera.WorldToScreenPoint(allUnits[i].transform.position);
+                    if (rect.Contains(screenPosition)) {
+                        Select(allUnits[i]);
+                    }
+                }
+
+                CurrentSelectionState = SelectionState.Frame;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            FrameImage.enabled = false;
+            if (ListOfSelected.Count > 0) {
+                CurrentSelectionState = SelectionState.UnitsSelected;
+            } else {
+                CurrentSelectionState = SelectionState.Other;
+            }
         }
     }
 
@@ -67,6 +131,7 @@ public class Management : MonoBehaviour {
             ListOfSelected[i].UnSelect();
         }
         ListOfSelected.Clear();
+        CurrentSelectionState = SelectionState.Other;
     }
 
     void UnHoverCurrent() {
