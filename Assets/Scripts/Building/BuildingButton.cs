2146d23970beeb06343517938a36afbe1e01cade
mishapushka <72805598+mishapushka@users.noreply.github.com>
2023-02-22 14:12:07 +0700

    Add menu photo cards

---
+++ Assets/Scripts/Building/BuildingButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingButton : MonoBehaviour
+{
+
+    public BuildingPlacer BuildingPlacer;
+    public GameObject BuildingPrefab;
+
+    public void TryBy() {
+        int price = BuildingPrefab.GetComponent<Building>().Price;
+
+        if (FindObjectOfType<Resources>().Money >= price) {
+            FindObjectOfType<Resources>().Money -= price;
+            BuildingPlacer.CreateBuilding(BuildingPrefab);
+        } else {
+            Debug.Log("Мало золота");
+        }
+    }
+}
