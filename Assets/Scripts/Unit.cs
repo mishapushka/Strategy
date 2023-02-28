f64f2c288cc2e51a2c9394081bf9688698c7a5d6
mishapushka <72805598+mishapushka@users.noreply.github.com>
2023-02-28 17:22:29 +0700

    Add Health Bar in unit

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -8,6 +8,18 @@ public class Unit : SelectableObject {
     public NavMeshAgent NavMeshAgent;
     public int Price;
     public int Health;
+    private int _maxHealth;
+
+    public GameObject HealthbarPrefab;
+    private HealthBar _healthBar;
+
+    public override void Start() {
+        base.Start();
+        _maxHealth = Health;
+        GameObject healthBar = Instantiate(HealthbarPrefab);
+        _healthBar = healthBar.GetComponent<HealthBar>();
+        _healthBar.Setup(transform);
+    }
 
     public override void WhenClickOnGround(Vector3 point) {
         base.WhenClickOnGround(point);
@@ -17,9 +29,14 @@ public class Unit : SelectableObject {
 
     public void TakeDamage(int damageValue) {
         Health -= damageValue;
+        _healthBar.SetHealth(Health, _maxHealth);
         if (Health <= 0) {
             // Die
             Destroy(gameObject);
         }
+    }
+
+    private void OnDestroy() {
+        Destroy(_healthBar.gameObject);
     }
 }
